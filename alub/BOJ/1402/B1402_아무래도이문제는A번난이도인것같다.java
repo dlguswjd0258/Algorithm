f28c7356f80a15b76e7d7039cdcc7d521a4ecc77
adlguswjda <adlguswjda@naver.com>
2021-12-08 15:25:08 +0900

    BOJ-1402 '아무래도이문제는A번난이도인것같다' [ 21/12/08 15:25:08 ] by Alub

---
+++ alub/BOJ/1402/B1402_아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,19 @@
+/**
+* 메모리: 11328 KB, 시간: 80 ms
+* 2021.12.08
+* by Alub
+*/
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(in.readLine());
+        while(T-- > 0){
+            in.readLine();
+            // 1의 개수는 무한히 넣을 수 있다..ㅎ..
+            System.out.println("yes");
+        }
+    }
+}
