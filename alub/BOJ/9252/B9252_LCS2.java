4a1175c936b3d13be4388f40c0761f06202c916b
adlguswjda <adlguswjda@naver.com>
2021-12-14 10:59:00 +0900

    BOJ-9252 'LCS 2' [ 21/12/14 10:58:59 ] by Alub

--- alub/BOJ/9252/B9252_LCS2.java
+++ alub/BOJ/9252/B9252_LCS2.java
@@ -1,43 +1,70 @@
 /**
-* 메모리: 60648 KB, 시간: 184 ms
+* 메모리: 15800 KB, 시간: 100 ms
 * 2021.12.14
 * by Alub
 */
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 
 public class Main {
 
 	static int aLen, bLen;
 	static char[] A, B;
-	static String[][] lcs;
+	static int[][] len;
 
 	public static void main(String[] args) throws Exception {
 		init();
 
-		getLcs();
+		int resLen = getLcsLen();
+		System.out.println(resLen);
 
-		System.out.println(lcs[aLen][bLen].length() + "\n" + lcs[aLen][bLen]);
+		if (resLen > 0) {
+			System.out.println(getLcs());
+		}
 	}
 
-	private static void getLcs() {
+	private static String getLcs() {
+		StringBuilder sb = new StringBuilder();
 
-		int a, b;
+		int c = bLen;
+		for (int r = aLen; r > 0; r--) {
+			if (len[r][c] == 0) {
+				break;
+			}
+
+			// 현재 위치에서 같은 열중에서 길이가 다를때까지 열을 감소
+			while (c > 0 && len[r][c] == len[r][c - 1]) {
+				c--;
+			}
+
+			// 현재 위치에서 A와 B의 알파벳이 같다면 알파벳 추가 다르다면 pass
+			if (A[r - 1] == B[c - 1]) {
+				sb.insert(0, B[--c]);
+			}
+
+		}
+
+		return sb.toString();
+	}
+
+	private static int getLcsLen() {
+		// A를 행 B를 열로 사용
 		for (int i = 1; i <= aLen; i++) {
 			for (int j = 1; j <= bLen; j++) {
-
-				// 문자가 다르면 이전 문자들 중 긴 문자 저장
 				if (A[i - 1] != B[j - 1]) {
-					a = lcs[i - 1][j].length();
-					b = lcs[i][j - 1].length();
-
-					lcs[i][j] = a > b ? lcs[i - 1][j] : lcs[i][j - 1];
+//					둘이 다른 문자연 이전 문자의 i-1과 j-1의 길이 중 큰 값
+					len[i][j] = getMathLen(len[i - 1][j], len[i][j - 1]);
 				} else {
-					lcs[i][j] = lcs[i - 1][j - 1] + B[j - 1];
+//					j번째 문자가 i번째와 같은 문자라면 (i-1, j-1의 길이 + 1)과 j-1의 길이 중 큰 값
+					len[i][j] = getMathLen(len[i - 1][j - 1] + 1, len[i][j - 1]);
 				}
 			}
 		}
+
+		return len[aLen][bLen];
+	}
+
+	private static int getMathLen(int a, int b) {
+		return Math.max(a, b);
 	}
 
 	private static void init() throws Exception {
@@ -48,12 +75,6 @@ public class Main {
 		aLen = A.length;
 		bLen = B.length;
 
-		lcs = new String[aLen + 1][bLen + 1];
-
-		// lcs 0행, 0열 초기화
-		Arrays.fill(lcs[0], "");
-		for (int i = 1; i <= aLen; i++) {
-			lcs[i][0] = "";
-		}
+		len = new int[aLen + 1][bLen + 1];
 	}
 }
